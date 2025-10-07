cc1e15d668340a84abb6ac60badb817af1397b7d
Tomás Alegre Sepúlveda <tas001@alumnos.ucn.cl>
2025-10-07 00:21:47 -0300

    refactor: simplify Unleash client module configuration and improve code structure
    - Update UnleashClientModuleAsyncOptions to remove unnecessary properties
    - Refactor forRootAsync and forRoot methods for clarity and consistency
    - Enhance provider definitions for better readability

--- src/unleash/interface/unleash-client-constructor.interface.ts
+++ src/unleash/interface/unleash-client-constructor.interface.ts
@@ -8,8 +8,4 @@ export interface UnleashClientConstructorInterface {
    * The core configuration object for the Unleash client, based on the `unleash-client` library's `UnleashConfig`.
    */
   config: UnleashConfig;
-  /**
-   * If `true`, the Unleash client will be registered as a global module, making it available across the entire application.
-   */
-  global?: boolean;
 }
